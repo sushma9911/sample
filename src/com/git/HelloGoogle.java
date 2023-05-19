b17ad8b010e149115801b78d9b08a495837c50b4
gondisushma <gondisushma11@gmail.com>
2023-05-19 17:34:52 +0530

    bcodechange

--- src/com/git/HelloGoogle.java
+++ src/com/git/HelloGoogle.java
@@ -1,7 +1,18 @@
 package com.git;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class HelloGoogle {
 	public static void main(String[] args) {
+		List li = new ArrayList();
+		li.add(10);
+		li.add(12);
+		for (Object object : li) {
+			System.out.println(object);
+			
+		}
 		System.out.println("HelloGoogle");
 	}
 	
